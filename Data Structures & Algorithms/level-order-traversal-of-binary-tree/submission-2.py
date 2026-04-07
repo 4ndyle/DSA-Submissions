65080528ba9673aa48fec15a1676fe4b60644c1e
Andy Le <136204727+4ndyle@users.noreply.github.com>
2026-04-06 22:06:43 -0700

    Add: level-order-traversal-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-2.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+Input:
+    - TreeNode: root 
+Output:
+    - List[List[int]]: sublists of values of nodes at each particular level (from L to R)
+Constraints:
+    - number of nodes: [0,1000]
+    - range of values: [-1000,1000]
+
+Plan: 
+BFs Search 
+"""
+
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        # bfs traversal 
+        queue = deque()
+        queue.append(root)
+        
+        res = []
+
+        # while there are nodes to process in the tree 
+        while queue: 
+            # get the number of nodes in the current level 
+            currLevelLength = len(queue)
+            level = []
+            
+            # process all nodes in the current level 
+            for i in range(currLevelLength):
+                currNode = queue.popleft()
+
+                if currNode:
+                    level.append(currNode.val)
+
+                    # add the left and right children of the current node to the queue to be processed later 
+                    queue.append(currNode.left)
+                    queue.append(currNode.right)
+                
+            # add the sublist of the current level to the result 
+            if level: res.append(level)
+            
+        return res
+
+
+
+
+
+        
