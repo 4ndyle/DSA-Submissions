2c7346b0fbcf683cf0243aca9cbc9f5a13a34e24
Andy Le <136204727+4ndyle@users.noreply.github.com>
2026-05-03 18:28:29 -0700

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,52 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+"""
+Input:
+    - List[Interval] : intervals 
+Output:
+    - bool : schedule has any conflicts or not 
+constraints:
+    - length of intervals: [0,500]
+    - range of values: [0,1000000]
+    - intervals[i].start < intervals[i].end 
+
+empty list: return True
+
+Plan: 
+1. Sort the list in ascending order by the start time 
+2. Iterate through each interval and check if the current interval start 
+is less than the previous intervals end time 
+    - if the current interval's start < prev end time, return False 
+3. Return True if all intervals do not conflict 
+
+Time: O(n logn)
+Space: O(1)
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        # sort the list of intervals by the start time 
+        intervals.sort(key = lambda currInterval: currInterval.start)
+        
+        for i in range(1, len(intervals)):
+            currInterval = intervals[i]
+
+            # check if the current interval start < prev interval end 
+            if currInterval.start < intervals[i-1].end:
+                return False
+
+        return True
+
+
+
+
+
