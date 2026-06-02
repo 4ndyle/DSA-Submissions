9094dca63a40932d58f2461fb20d982c3bc528dd
Andy Le <136204727+4ndyle@users.noreply.github.com>
2026-06-02 08:27:35 -0700

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,47 @@
+"""
+Input:
+    - List[List[int]] : grid
+Output:
+    - int : number of islands in the graph 
+Constraints:
+    - length of list: [1,100]
+    - grid[i]: 0, 1
+    - island: 1's that are connected adjacently horizontally or vertically 
+
+Plan:
+Go through each node in the graph and perform a dfs
+when we encounter an 1 to mark the 1's that form an island
+"""
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # up, down, left, right
+        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        visited = set()
+
+        numberOfIslands = 0 
+
+        def dfs(row, col):  
+            # base case
+            if (row, col) in visited:
+                return
+            
+            outOfBounds = row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0])
+            if outOfBounds or grid[row][col] == "0":
+                return 
+
+            # mark the current node as visited
+            visited.add((row,col))
+
+            # recursive case 
+            # call dfs on neighbors 
+            for dr, dc in directions:
+                dfs(dr + row, dc + col)
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1" and (row,col) not in visited:
+                    dfs(row, col)
+                    numberOfIslands += 1
+
+        return numberOfIslands
