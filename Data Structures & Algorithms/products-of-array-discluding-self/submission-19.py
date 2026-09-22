33767a21bacf89f7d7d5ecfedaa6bbd4996f5c10
Andy Le <136204727+4ndyle@users.noreply.github.com>
2026-09-22 09:54:23 -0700

    Add: products-of-array-discluding-self - submission-19

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-19.py
@@ -0,0 +1,40 @@
+"""
+Input:
+    - List[int] : nums
+Output:
+    - List[int] : product of all elements except nums[i]
+Constraints:
+    - each product fits in a 32-bit integer 
+    - length of nums: [2,100000]
+    - values of nums: [-30,30]
+
+Using division modulo
+1. Get product of all numbers 
+    - keep count of number of zeros 
+        if there are more than 2 zeros, all nums[i] = 0
+        otherwise, skip the one zero when calculating products
+2. Create list of len(nums) 0's and calculate the products 
+    - reuslt[i] = totalProduct / nums[i]
+
+Using prefix and suffix 
+"""
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # find the prefix
+        result = []
+        prefixProduct = 1
+
+        for num in nums:
+            result.append(prefixProduct)
+            prefixProduct *= num
+
+        # find the suffix 
+        suffixProduct = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= suffixProduct
+            suffixProduct *= nums[i]
+
+        return result
+        
