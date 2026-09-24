ce3c22aecfb34bdf44f66d74f1635d1c7f75b825
Andy Le <136204727+4ndyle@users.noreply.github.com>
2026-09-24 08:59:21 -0700

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,81 @@
+"""
+Input:
+    - int : numCourses
+    - List[List[int]] : prerequisites 
+Output:
+    - bool : true if all courses can be completed
+Constraints:
+    - [a,b] : course b MUST be taken before course a
+    - possible lengths of prerequisites: [0,1000] 
+    - possible values of prequisites: [0,numCourses]
+    - prerequisite[i].length = 2
+    - possible values of numCourses: [1,100]
+
+Plan: Create an adjacency list and map each course to their prereq
+{
+    course : set(prerequisites)
+}
+
+Example 1:
+{
+    0 : set(1)
+    1 : set(0)
+}
+
+Iterate through and check if course 0 needs course 1 and course 1 needs course 0
+
+1. Create variables
+    - courseMap = {}
+2. for course, prereq in prerequisites:
+        courseMap[course] = courseMap.get(course, set()).add(prereq)
+3. for course, prereqList in courseMap:
+        for prereq in prereqList:
+            if course in courseMap[prereq]:
+                return False
+4. Return True
+"""
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # map each course to prereq list (adjacency list)
+        courseMap = {}
+
+        for course, prereq in prerequisites:
+            if course not in courseMap:
+                courseMap[course] = set()
+            if prereq not in courseMap:
+                courseMap[prereq] = set()
+
+            courseMap[course].add(prereq)
+
+        # go through adjacency list and check for a cycle
+        visited = set()
+        coursePossibility = {}
+
+        def dfsHelper(node):
+            if node in coursePossibility:
+                return coursePossibility[node]
+            if node in visited:
+                return "Contradiction"
+
+            # mark node as visited
+            visited.add(node)
+
+            for neighbor in courseMap[node]:
+                if dfsHelper(neighbor) == "Contradiction":
+                    coursePossibility[node] = "Contradiction"
+                    return "Contradiction"
+            
+            visited.remove(node)
+
+            coursePossibility[node] = "Possible"
+            return "Possible"
+
+        for course in courseMap:            
+            if dfsHelper(course) == "Contradiction":
+                return False
+            
+            visited = set()
+        
+        return True
+        
